bdfc82fa0f1e2b46263884af6b1a4f4d6d1a0f15
Preeti Gusain <preetigusain9173@gmail.com>
2024-07-18 00:10:53 +0530

    Added a basic navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { InputComponent } from './input/input.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    InputComponent
+    InputComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
